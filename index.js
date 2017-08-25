e8bebb7aeb6197ed6d6c984e2337c7580df4e70f
1amageek <norikazu-muramoto@cookpad.com>
2017-08-25 13:30:22 +0900

    bugfix

--- index.js
+++ index.js
@@ -2,41 +2,44 @@ const functions = require('firebase-functions');
 const _ = require('lodash');
 const request = require('request-promise');
 
-exports.to = (type, properties) => {
+const dressing = {
+	put: (type, properties) => {
+		if (!type) {
+			console.log("[Dressing] *** error: 'type' is not defined.");
+			return
+		}
 
-	if (!type) {
-		console.log("[Dressing] *** error: 'type' is not defined.");
-		return
+		const ignore = properties || [];
+		const path = `/{version}/${type}/{id}/`;
+
+		return functions.database.ref(path)
+		.onWrite(event => {
+			const version		= event.params.version;
+			const id				= event.params.id;
+			const data 			= event.data.val();
+
+			console.log(`${version}/${type}/${id}`, data);
+
+			let elasticSearchConfig = functions.config().elasticsearch;
+			let elasticSearchUrl = elasticSearchConfig.url + `${version}/${type}/${id}`;
+			let elasticSearchMethod = data ? 'POST' : 'DELETE';
+
+			let elasticsearchRequest = {
+				method: elasticSearchMethod,
+				uri: elasticSearchUrl,
+				auth: {
+					username: elasticSearchConfig.username,
+					password: elasticSearchConfig.password,
+				},
+				body: _.omit(data, ignore),
+				json: true
+			};
+
+			return request(elasticsearchRequest).then(response => {
+				console.log('Elasticsearch response', response);
+			})
+		});
 	}
-
-	const ignore = properties || [];
-	const path = `/{version}/${type}/{id}/`;
-
-	return functions.database.ref(path)
-	.onWrite(event => {
-		const version		= event.params.version;
-		const id				= event.params.id;
-		const data 			= event.data.val();
-
-		console.log(`${version}/${type}/${id}`, data);
-
-		let elasticSearchConfig = functions.config().elasticsearch;
-		let elasticSearchUrl = elasticSearchConfig.url + `${version}/${type}/${id}`;
-		let elasticSearchMethod = data ? 'POST' : 'DELETE';
-
-		let elasticsearchRequest = {
-			method: elasticSearchMethod,
-			uri: elasticSearchUrl,
-			auth: {
-				username: elasticSearchConfig.username,
-				password: elasticSearchConfig.password,
-			},
-			body: _.omit(data, ignore),
-			json: true
-		};
-
-		return request(elasticsearchRequest).then(response => {
-			console.log('Elasticsearch response', response);
-		})
-	});
 }
+
+module.exports = dressing;
